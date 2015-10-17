64f16f22d4622db9ebad1ce34f30f32c91e5224b
victor95 <victor95@victorpalomocastro@gmail.com>
2015-10-17 19:17:10 -0300

    Improving the instruction inside auto_scopes config

--- config/initializers/auto_scopes.rb
+++ config/initializers/auto_scopes.rb
@@ -1,7 +1,12 @@
-# associations_location is where autoscopes must be defined
-# prefix is 
+# auto_scopes_location: where autoscopes must be defined
+# 
+# create_scope_for_association: in each association the parent model
+# a scope with id is create this option set this behavior
+# 
+# scope_association_prefix: in each association the parent model
+# this is the prefix added to them
 AutoScopes.configure do |c|
 	# c.auto_scopes_location         = 'config/auto_scopes'
-	# c.scope_association_prefix     = 'by'
 	# c.create_scope_for_association = true
+	# c.scope_association_prefix     = 'by'
 end
